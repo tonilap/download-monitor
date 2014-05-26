ded7b19b4938f5418e7ed635f05539d605419465
tlapuyade <tlapuyade@worldsensing.com>
2014-05-26 17:27:20 +0200

    Add zmq publisher over tcp

--- arrival-notification.js
+++ arrival-notification.js
@@ -1,4 +1,19 @@
 "use-strict"
 const
   fs = require('fs'),
-  filename = process.argv[2];
+  zmq = require('zmq'),
+  directory = process.argv[2];
+  publisher = zmq.socket('pub');
+
+fs.watch(directory, function(event, filename) {
+  // send message to subscribers
+  publisher.send(JSON.stringify({
+  	type: 'new',
+  	file: filename,
+  	timestamp: Date.now()
+  }));
+
+  publisher.bind('tcp://*:5432', function(err) {
+  	console.log('Listening for subscribers...')
+  });
+});
