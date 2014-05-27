694c368617c31845eb185b107829599127872613
tlapuyade <tlapuyade@worldsensing.com>
2014-05-27 11:30:25 +0200

    change zmq bind for bindSync

--- arrival-notification.js
+++ arrival-notification.js
@@ -13,7 +13,7 @@ fs.watch(directory, function(event, filename) {
   	timestamp: Date.now()
   }));
 
-  publisher.bind('tcp://*:5910', function(err) {
+  publisher.bindSync('tcp://*:5910', function(err) {
   	console.log('Listening for subscribers...')
   });
 });
